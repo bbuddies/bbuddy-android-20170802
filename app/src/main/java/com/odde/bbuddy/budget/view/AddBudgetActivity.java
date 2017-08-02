104c42948415a70f94f44dbdd567c20e85d0cd79
Joseph Yao <joseph.yao.ruozhou@gmail.com>
2017-08-02 15:49:31 +0800

    add budget: data binding and navigation for editable budget

--- app/src/main/java/com/odde/bbuddy/budget/view/AddBudgetActivity.java
+++ app/src/main/java/com/odde/bbuddy/budget/view/AddBudgetActivity.java
@@ -3,8 +3,6 @@ package com.odde.bbuddy.budget.view;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.Button;
 
 import com.odde.bbuddy.R;
 import com.odde.bbuddy.budget.viewmodel.EditableBudget;
@@ -30,13 +28,5 @@ public class AddBudgetActivity extends AppCompatActivity {
         createActivityComponentBy(this).inject(this);
 
         setContentView(viewBinder.inflateAndBind(R.layout.activity_add_budget, editableBudget));
-
-        Button saveButton = (Button)findViewById(R.id.confirm);
-        saveButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                finish();
-            }
-        });
     }
 }
--- app/src/main/java/com/odde/bbuddy/budget/viewmodel/EditableBudget.java
+++ app/src/main/java/com/odde/bbuddy/budget/viewmodel/EditableBudget.java
@@ -1,6 +1,9 @@
 package com.odde.bbuddy.budget.viewmodel;
 
 
+import android.util.Log;
+
+import com.odde.bbuddy.budget.view.BudgetsNavigation;
 import com.odde.bbuddy.di.scope.ActivityScope;
 
 import org.robobinding.annotation.PresentationModel;
@@ -11,6 +14,35 @@ import javax.inject.Inject;
 @ActivityScope
 public class EditableBudget {
 
+    private final BudgetsNavigation budgetsNavigation;
+
     @Inject
-    public EditableBudget() { }
+    public EditableBudget(BudgetsNavigation budgetsNavigation) {
+        this.budgetsNavigation = budgetsNavigation;
+    }
+
+    private String month;
+    private String amount;
+
+    public void add() {
+        Log.d("month", month);
+        Log.d("amount", amount);
+        budgetsNavigation.navigate();
+    }
+
+    public String getMonth() {
+        return month;
+    }
+
+    public void setMonth(String month) {
+        this.month = month;
+    }
+
+    public String getAmount() {
+        return amount;
+    }
+
+    public void setAmount(String amount) {
+        this.amount = amount;
+    }
 }
